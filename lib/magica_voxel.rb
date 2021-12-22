e238e9e1ae6a72a8b6228e0f79e6bbb122c4c9d7
蒼時弦也 <elct9620@frost.tw>
2021-12-22 13:31:08 +0800

    Initialize Project

---
+++ lib/magica_voxel.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'magica_voxel/version'
+
+module MagicaVoxel
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/magica_voxel/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MagicaVoxel
+  VERSION = '0.1.0'
+end
