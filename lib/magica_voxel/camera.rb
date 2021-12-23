4ac84ef7694962e10f1da3aaa0d7969b90c663c9
蒼時弦也 <elct9620@frost.tw>
2021-12-23 22:11:06 +0800

    Add MagicaVoxel::Camera related attributes

--- lib/magica_voxel/camera.rb
+++ lib/magica_voxel/camera.rb
@@ -8,11 +8,68 @@ module MagicaVoxel
     # @since 0.1.0
     attr_reader :id, :attributes
 
+    # Mode
+    #
+    # @return [String]
+    #
+    # @since 0.3.0
+    def mode
+      @attributes['_mode']
+    end
+
+    # Focus
+    #
+    # @return [MagicalVoxel::Vector3]
+    #
+    # @since 0.3.0
+    def focus
+      @focus ||= Vector3.new(*@attributes['_focus'].split.map(&:to_f))
+    end
+
+    # Angle
+    #
+    # @return [MagicalVoxel::Vector3]
+    #
+    # @since 0.3.0
+    def angle
+      @angle ||= Vector3.new(*@attributes['_angle'].split.map(&:to_f))
+    end
+
+    # Radius
+    #
+    # @return [Number]
+    #
+    # @since 0.3.0
+    def radius
+      @radius ||= @attributes['_radius'].to_i
+    end
+
+    # Frustum
+    #
+    # @return [Number]
+    #
+    # @since 0.3.0
+    def frustum
+      @frustum ||= @attributes['_frustum'].to_f
+    end
+
+    # Field of View
+    #
+    # @return [Number]
+    #
+    # @since 0.3.0
+    def fov
+      @fov ||= @attributes['_fov'].to_i
+    end
+
     # :nodoc:
     #
     # @since 0.1.0
     def inspect
-      "#<MagicaVoxel::Camera id=#{id}>"
+      "#<MagicaVoxel::Camera id=#{id}, mode=#{mode}, " \
+        "focus=#{focus}, angle=#{angle}, " \
+        "radius=#{radius}, frustum=#{frustum}, " \
+        "fov=#{fov}>"
     end
 
     private
--- spec/magica_voxel/camera_spec.rb
+++ spec/magica_voxel/camera_spec.rb
@@ -5,9 +5,19 @@ RSpec.describe MagicaVoxel::Camera do
 
   let(:content) do
     "\x01\x00\x00\x00" \
-      "\x01\x00\x00\x00" \
+      "\x06\x00\x00\x00" \
       "\x05\x00\x00\x00_mode" \
-      "\x05\x00\x00\x00_test"
+      "\x04\x00\x00\x00pers" \
+      "\x06\x00\x00\x00_focus" \
+      "\x05\x00\x00\x000 0 0" \
+      "\x06\x00\x00\x00_angle" \
+      "\x05\x00\x00\x001 1 1" \
+      "\x07\x00\x00\x00_radius" \
+      "\x02\x00\x00\x0010" \
+      "\x08\x00\x00\x00_frustum" \
+      "\x05\x00\x00\x000.421" \
+      "\x04\x00\x00\x00_fov" \
+      "\x02\x00\x00\x0045"
   end
 
   describe '#id' do
@@ -16,9 +26,44 @@ RSpec.describe MagicaVoxel::Camera do
     it { is_expected.to eq(1) }
   end
 
+  describe '#focus' do
+    subject { camera.focus }
+
+    it { is_expected.to eq(MagicaVoxel::Vector3.new(0.0, 0.0, 0.0)) }
+  end
+
+  describe '#angle' do
+    subject { camera.angle }
+
+    it { is_expected.to eq(MagicaVoxel::Vector3.new(1.0, 1.0, 1.0)) }
+  end
+
+  describe '#radius' do
+    subject { camera.radius }
+
+    it { is_expected.to eq(10) }
+  end
+
+  describe '#frustum' do
+    subject { camera.frustum }
+
+    it { is_expected.to eq(0.421) }
+  end
+
+  describe '#fov' do
+    subject { camera.fov }
+
+    it { is_expected.to eq(45) }
+  end
+
   describe '#inspect' do
     subject { camera.inspect }
 
     it { is_expected.to match(/id=1/) }
+    it { is_expected.to match(/focus=[^,]*x=0.0, y=0.0, z=0.0/) }
+    it { is_expected.to match(/angle=[^,]*x=1.0, y=1.0, z=1.0/) }
+    it { is_expected.to match(/radius=10/) }
+    it { is_expected.to match(/frustum=0.421/) }
+    it { is_expected.to match(/fov=45/) }
   end
 end
