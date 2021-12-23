a2ea8f079e58b5625dcfd4db1e30cc7995be98bb
蒼時弦也 <elct9620@frost.tw>
2021-12-23 20:43:52 +0800

    Add MagicaVoxel::Material related properties

--- lib/magica_voxel/material.rb
+++ lib/magica_voxel/material.rb
@@ -8,14 +8,89 @@ module MagicaVoxel
     # @since 0.1.0
     attr_reader :id, :properties
 
+    # Material Type
+    #
+    # @return [String]
+    #
+    # @since 0.1.0
+    def type
+      @type ||= @properties['_type'][1..-1]
+    end
+
+    # Weight
+    #
+    # @return [Number]
+    #
+    # @since 0.2.0
+    def weight
+      @weight ||= @properties['_weight'].to_f
+    end
+
+    # Roughness
+    #
+    # @return [Number]
+    #
+    # @since 0.2.0
+    def roughness
+      @roughness ||= @properties['_rough'].to_f
+    end
+
+    # Specular
+    #
+    # @return [Number]
+    #
+    # @since 0.2.0
+    def specular
+      @specular ||= @properties['_spec'].to_f
+    end
+
+    # Index of Refraction
+    #
+    # @return [Number]
+    #
+    # @since 0.2.0
+    def ior
+      @ior ||= @properties['_ior'].to_f
+    end
+
+    # TODO: Att
+    #
+    # @return [Number]
+    #
+    # @since 0.2.0
+    def att
+      @att ||= @properties['_att'].to_f
+    end
+
+    # TODO: Flux
+    #
+    # @return [Number]
+    #
+    # @since 0.2.0
+    def flux
+      @flux ||= @properties['_flux'].to_f
+    end
+
+    # TODO: Plastic
+    #
+    # @return [TureClass|FalseClass]
+    #
+    # @since 0.1.0
+    def plastic?
+      @properties['_plastic'] == '1'
+    end
+
     # :nodoc:
     #
     # @since 0.1.0
     def inspect
-      "#<MagicaVoxel::Material id=#{id}>"
+      '#<MagicaVoxel::Material ' \
+        "id=#{id}, type=#{type}, " \
+        "weight=#{weight}, roughness=#{roughness}, " \
+        "specular=#{specular}, ior=#{ior}, " \
+        "att=#{att}, flux=#{flux}, " \
+        "plastic=#{plastic?}>"
     end
-
-    # TODO: Expose Properties
 
     private
 
--- spec/magica_voxel/material_spec.rb
+++ spec/magica_voxel/material_spec.rb
@@ -5,9 +5,23 @@ RSpec.describe MagicaVoxel::Material do
 
   let(:content) do
     "\x01\x00\x00\x00" \
-      "\x01\x00\x00\x00" \
+      "\x08\x00\x00\x00" \
       "\x05\x00\x00\x00_type" \
-      "\x05\x00\x00\x00_emit"
+      "\x05\x00\x00\x00_emit" \
+      "\x07\x00\x00\x00_weight" \
+      "\x03\x00\x00\x000.3" \
+      "\x06\x00\x00\x00_rough" \
+      "\x03\x00\x00\x000.1" \
+      "\x05\x00\x00\x00_spec" \
+      "\x03\x00\x00\x000.2" \
+      "\x04\x00\x00\x00_ior" \
+      "\x03\x00\x00\x000.3" \
+      "\x04\x00\x00\x00_att" \
+      "\x03\x00\x00\x000.1" \
+      "\x05\x00\x00\x00_flux" \
+      "\x03\x00\x00\x000.2" \
+      "\x08\x00\x00\x00_plastic" \
+      "\x03\x00\x00\x001"
   end
 
   describe '#id' do
@@ -16,9 +30,63 @@ RSpec.describe MagicaVoxel::Material do
     it { is_expected.to eq(1) }
   end
 
+  describe '#type' do
+    subject { material.type }
+
+    it { is_expected.to eq('emit') }
+  end
+
+  describe '#weight' do
+    subject { material.weight }
+
+    it { is_expected.to eq(0.3) }
+  end
+
+  describe '#roughness' do
+    subject { material.roughness }
+
+    it { is_expected.to eq(0.1) }
+  end
+
+  describe '#specular' do
+    subject { material.specular }
+
+    it { is_expected.to eq(0.2) }
+  end
+
+  describe '#ior' do
+    subject { material.ior }
+
+    it { is_expected.to eq(0.3) }
+  end
+
+  describe '#att' do
+    subject { material.att }
+
+    it { is_expected.to eq(0.1) }
+  end
+
+  describe '#flux' do
+    subject { material.flux }
+
+    it { is_expected.to eq(0.2) }
+  end
+
+  describe '#plastic?' do
+    it { is_expected.to be_plastic }
+  end
+
   describe '#inspect' do
     subject { material.inspect }
 
     it { is_expected.to match(/id=1/) }
+    it { is_expected.to match(/type=emit/) }
+    it { is_expected.to match(/weight=0.3/) }
+    it { is_expected.to match(/roughness=0.1/) }
+    it { is_expected.to match(/specular=0.2/) }
+    it { is_expected.to match(/ior=0.3/) }
+    it { is_expected.to match(/att=0.1/) }
+    it { is_expected.to match(/flux=0.2/) }
+    it { is_expected.to match(/plastic=true/) }
   end
 end
