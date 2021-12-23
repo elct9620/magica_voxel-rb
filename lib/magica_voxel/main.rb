12a9ee8c48de043186d150a93c3c8ae5404fb9e8
蒼時弦也 <elct9620@frost.tw>
2021-12-23 17:39:33 +0800

    Add models filter to create SIZE and XYZI pair

--- lib/magica_voxel/main.rb
+++ lib/magica_voxel/main.rb
@@ -5,5 +5,12 @@ module MagicaVoxel
   #
   # @since 0.1.0
   class Main < Chunk
+    # @return [Array<Array<MagicaVoxel::Size|MagicalVoxel::Model>>]
+    #
+    # @since 0.1.0
+    def models
+      select { |chunk| chunk.is_a?(MagicaVoxel::Size) || chunk.is_a?(MagicaVoxel::Model) }
+        .each_slice(2)
+    end
   end
 end
